ab96f0018e3de3afb27597540af1e8a946498d67
jura <jglotovyn@gmail.com>
2025-06-01 19:03:26 +0300

    ADD: function comments

--- delay/delay.h
+++ delay/delay.h
@@ -3,8 +3,22 @@
 
 #include <stdint.h>
 
+/*Brief: Blocking delay in seconds
+ * [in] - seconds
+ * [out] - none
+ * */
 void DelaySec(uint32_t sec);
+
+/*Brief: Blocking delay in milliseconds
+ * [in] - milliseconds
+ * [out] - none
+ * */
 void DelayMs(uint32_t msec);
+
+/*Brief: Blocking delay in microseconds
+ * [in] - microseconds
+ * [out] - none
+ * */
 void DelayUs(uint32_t usec);
 
 #endif /* DELAY_H */
