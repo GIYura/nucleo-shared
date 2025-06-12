b05751fc7bff25eadf7eaac428021db286477138
jura <jglotovyn@gmail.com>
2025-06-12 21:59:35 +0300

    ADD: usart

--- uart/uart.c
+++ uart/uart.c
@@ -1,38 +1,122 @@
+#include <assert.h>
 #include <stdbool.h>
+#include <stddef.h>
 
 #include "uart.h"
 #include "gpio.h"
 
-#include "stm32f411xe.h"
+static Gpio_t m_gpio;
 
-static Gpio_t m_gpioTx;
+static bool m_initialized = false;
 
-static bool initialized = false;
-
-void UartInit(void)
+/*Brief: Send single character */
+static void PutChar(const Usart_t* const obj, char ch)
 {
-    GpioInit(&m_gpioTx, PA_9, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_7);
-
-    /*uart clock enable*/
-    RCC->AHB2ENR |= (1 << 5);
-
-    /*enable usart and tx*/
-    USART1->CR1 |= (1 << 13) | (1 << 3);
-
-    USART1->BRR = 0x1234;
-
-    initialized = true;
+    while (!(obj->usart->SR & USART_SR_TXE)){}
+    {
+        obj->usart->DR = ch;
+    }
 }
 
-void UartWrite(const uint8_t* const buff, uint8_t size)
+/*Brief: Converts baud rate in to register value */
+static uint16_t BaudRateToRegisterValue(BAUD_RATE baud)
 {
-    if (!initialized)
+    uint32_t baudrate = 0;
+
+    switch (baud)
+    {
+        case BAUD_1200:
+            baudrate = 1200;
+            break;
+
+        case BAUD_2400:
+            baudrate = 2400;
+            break;
+
+        case BAUD_9600:
+            baudrate = 9600;
+            break;
+
+        case BAUD_38400:
+            baudrate = 38400;
+            break;
+
+        case BAUD_57600:
+            baudrate = 57600;
+            break;
+
+        case BAUD_115200:
+            baudrate = 115200;
+            break;
+
+        case BAUD_230400:
+            baudrate = 230400;
+            break;
+
+        case BAUD_460800:
+            baudrate = 460800;
+            break;
+
+        case BAUD_921600:
+            baudrate = 921600;
+            break;
+
+        default:
+            break;
+    }
+
+    float usartDiv = (float)SystemCoreClock / (16.0f * baudrate);
+
+    uint32_t mantissa = (uint32_t)usartDiv;
+    uint32_t fraction = (uint32_t)((usartDiv - mantissa) * 16.0f + 0.5f);
+
+    if (fraction > 15)
+    {
+        mantissa += 1;
+        fraction = 0;
+    }
+
+    return (mantissa << 4) | (fraction & 0x0F);
+}
+
+void UartInit(Usart_t* const obj, BAUD_RATE baud)
+{
+    assert(baud < BAUD_COUNT);
+    assert(obj != NULL);
+
+    /* init PA9 (USART1 TX) */
+    GpioInit(&m_gpio, PA_9, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_7);
+
+    /* link USART1 instance */
+    obj->usart = USART1;
+
+    /* USART clock enable */
+    RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+
+    obj->usart->BRR = BaudRateToRegisterValue(baud);
+
+    /* enable transmitter */
+    obj->usart->CR1 |= USART_CR1_TE;
+
+    /* enable USART */
+    obj->usart->CR1 |= USART_CR1_UE;
+
+    m_initialized = true;
+}
+
+void UartWrite(const Usart_t* const obj, const char* buff)
+{
+    assert(obj != NULL);
+    assert(buff != NULL);
+
+    if (!m_initialized)
     {
         return;
     }
 
-    for (uint8_t i = 0; i < size; i++)
+    while (*buff)
     {
-        USART1->DR = buff[i];
+        PutChar(obj, *buff);
+        buff++;
     }
 }
--- uart/uart.h
+++ uart/uart.h
@@ -3,8 +3,41 @@
 
 #include <stdint.h>
 
-void UartInit(void);
+#include "stm32f411xe.h"
 
-void UartWrite(const uint8_t* const buff, uint8_t size);
+/* USART baud rate supported */
+typedef enum
+{
+    BAUD_1200 = 0,
+    BAUD_2400,
+    BAUD_9600,
+    BAUD_19200,
+    BAUD_38400,
+    BAUD_57600,
+    BAUD_115200,
+    BAUD_230400,
+    BAUD_460800,
+    BAUD_921600,
+    BAUD_COUNT
+} BAUD_RATE;
+
+typedef struct
+{
+    USART_TypeDef* usart;
+} Usart_t;
+
+/*Brief: USART initialization
+ * [in] - obj - pointer to USART object
+ * [in] - baud - baud rate
+ * [out] - none
+ * */
+void UartInit(Usart_t* const obj, BAUD_RATE baud);
+
+/*Brief: USART send buffer
+ * [in] - obj - pointer to USART object
+ * [in] - buff - pointer to string
+ * [out] - none
+ * */
+void UartWrite(const Usart_t* const obj, const char* buff);
 
 #endif /* UART_H */
