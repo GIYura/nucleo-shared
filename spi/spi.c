9cd0be1af73c37ba4f2c6dfd009a72d2dc7bf5b3
jura <jglotovyn@gmail.com>
2025-08-10 21:29:35 +0300

    ADD: spi driver

--- spi/spi.c
+++ spi/spi.c
@@ -1,8 +1,11 @@
 #include <assert.h>
 #include <stddef.h>
 
-#include "gpio.h"
+#include "delay.h"
 #include "spi.h"
+#include "ignore.h"
+
+#define WAIT_FLAG_TIMEOUT_MAX   100 /* us */
 
 #define SPI_1_CLOCK_ENABLE (RCC->APB2ENR |= (RCC_APB2ENR_SPI1EN))
 #define SPI_2_CLOCK_ENABLE (RCC->APB1ENR |= (RCC_APB1ENR_SPI2EN))
@@ -10,16 +13,166 @@
 #define SPI_4_CLOCK_ENABLE (RCC->APB2ENR |= (RCC_APB2ENR_SPI4EN))
 #define SPI_5_CLOCK_ENABLE (RCC->APB2ENR |= (RCC_APB2ENR_SPI5EN))
 
-typedef struct
+static void SpiGpioInit(Spi_t* const obj, PIN_NAMES miso, PIN_NAMES mosi, PIN_NAMES sck);
+
+static void SpiMode(const Spi_t* const obj, SPI_POLARITY polarity, SPI_PHASE phase);
+static void SpiFormat(const Spi_t* const obj);
+
+/*Brief: SPI speed
+ * [in] - obj - pointer to SPI object
+ * [in] - deriredFrequencyHz - desired SPI frequency in Hz
+ * [out] - uint32_t value - actual SPI frequency in Hz
+ * */
+static uint32_t SpiSpeed(const Spi_t* const obj, uint32_t deriredFrequencyHz);
+static void SpiClockEnable(const Spi_t* const obj);
+static void SpiEnable(const Spi_t* const obj);
+static void SpiDisable(const Spi_t* const obj);
+
+static bool WaitFlagTimeout(volatile uint32_t* reg, uint32_t flag, bool state, uint32_t timeoutUs);
+static void SpiClearOverrun(const Spi_t* const obj);
+
+void SpiInit(Spi_t* const obj, SPI_NAMES name, SPI_POLARITY polarity, SPI_PHASE phase, uint32_t deriredFrequencyHz)
 {
-    Gpio_t mosi;
-    Gpio_t miso;
-    Gpio_t sck;
-} SpiGpio_t;
+    assert(obj != NULL);
+    assert(deriredFrequencyHz != 0);
 
-static SpiGpio_t m_gpio;
+    uint32_t actualFreq = 0;
 
-void SpiMode(Spi_t* const obj, SPI_POLARITY polarity, SPI_PHASE phase)
+    obj->name = name;
+    obj->initialized = false;
+
+    switch (name)
+    {
+        case SPI_1:
+            SpiGpioInit(obj, PA_6, PA_7, PA_5);
+
+            obj->instance = SPI1;
+
+            break;
+
+        case SPI_2:
+            SpiGpioInit(obj, PB_14, PB_15, PB_13);
+
+            obj->instance = SPI2;
+
+            break;
+
+        case SPI_3:
+            SpiGpioInit(obj, PB_4, PB_5, PB_3);
+
+            obj->instance = SPI3;
+
+            break;
+
+        case SPI_4:
+            SpiGpioInit(obj, PE_5, PE_6, PE_4);
+
+            obj->instance = SPI4;
+
+            break;
+
+        case SPI_5:
+            SpiGpioInit(obj, PE_13, PE_14, PE_12);
+
+            obj->instance = SPI5;
+
+            break;
+
+        default:
+            assert(false);
+            break;
+    }
+
+    SpiClockEnable(obj);
+
+    SpiMode(obj, polarity, phase);
+
+    SpiFormat(obj);
+
+    actualFreq = SpiSpeed(obj, deriredFrequencyHz);
+
+    SpiEnable(obj);
+
+    SpiClearOverrun(obj);
+
+    obj->initialized = true;
+}
+
+void SpiDeinit(Spi_t* const obj)
+{
+    obj->initialized = false;
+
+    SpiDisable(obj);
+}
+
+bool SpiTransfer(Spi_t* const obj, const uint8_t* const txBuffer, uint8_t* const rxBuffer, uint8_t size)
+{
+    assert(obj != NULL);
+
+    if (!obj->initialized)
+    {
+        return false;
+    }
+
+    uint32_t dummy = 0;
+
+    for (uint8_t i = 0; i < size; i++)
+    {
+        if (!WaitFlagTimeout(&obj->instance->SR, SPI_SR_TXE, 1, WAIT_FLAG_TIMEOUT_MAX))
+        {
+            goto timeout;
+        }
+
+        obj->instance->DR = (txBuffer != NULL) ? txBuffer[i] : 0xFF;
+
+        if (!WaitFlagTimeout(&obj->instance->SR, SPI_SR_RXNE, 1, WAIT_FLAG_TIMEOUT_MAX))
+        {
+            goto timeout;
+        }
+
+        if (rxBuffer != NULL)
+        {
+            rxBuffer[i] = obj->instance->DR;
+        }
+        else
+        {
+            dummy = obj->instance->DR;
+            IGNORE(dummy);
+        }
+    }
+
+    if (!WaitFlagTimeout(&obj->instance->SR, SPI_SR_BSY, 0, WAIT_FLAG_TIMEOUT_MAX))
+    {
+        goto timeout;
+    }
+
+    return true;
+
+timeout:
+    SpiClearOverrun(obj);
+
+    return false;
+}
+
+static void SpiGpioInit(Spi_t* const obj, PIN_NAMES miso, PIN_NAMES mosi, PIN_NAMES sck)
+{
+    assert(obj != NULL);
+
+    if (obj->name == SPI_3 || obj->name == SPI_5)
+    {
+        GpioInit(&obj->gpio.miso, miso, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_6);
+        GpioInit(&obj->gpio.mosi, mosi, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_6);
+        GpioInit(&obj->gpio.sck, sck, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_6);
+    }
+    else
+    {
+        GpioInit(&obj->gpio.miso, miso, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_5);
+        GpioInit(&obj->gpio.mosi, mosi, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_5);
+        GpioInit(&obj->gpio.sck, sck, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_5);
+    }
+}
+
+static void SpiMode(const Spi_t* const obj, SPI_POLARITY polarity, SPI_PHASE phase)
 {
     assert(obj != NULL);
 
@@ -42,209 +195,150 @@ void SpiMode(Spi_t* const obj, SPI_POLARITY polarity, SPI_PHASE phase)
     }
 }
 
-void SpiFormat(Spi_t* const obj)
+static void SpiFormat(const Spi_t* const obj)
 {
     assert(obj != NULL);
-
-    /* enable full duplex */
-    obj->instance->CR1 &= ~SPI_CR1_RXONLY;
-
-    /* MSB first */
-    obj->instance->CR1 &= ~SPI_CR1_LSBFIRST;
 
     /* master mode */
-    obj->instance->CR1 |= SPI_CR1_MSTR;
+    obj->instance->CR1 |= (SPI_CR1_MSTR);
 
-    /* 8 bit data */
-    obj->instance->CR1 &= ~SPI_CR1_DFF;
+    /* MSB transmitted first */
+    obj->instance->CR1 &= ~(SPI_CR1_LSBFIRST);
 
-    /* software slave management enabled */
-    obj->instance->CR1 |= SPI_CR1_SSM;
-    obj->instance->CR1 |= SPI_CR1_SSI;
+    /*Software slave management enabled  */
+    obj->instance->CR1 |= (SPI_CR1_SSM);
+    obj->instance->CR1 |= (SPI_CR1_SSI);
+
+    /* Full duplex (Transmit and receive) */
+    obj->instance->CR1 &= ~(SPI_CR1_RXONLY);
+
+    /* 8-bit data frame format is selected for transmission/reception */
+    obj->instance->CR1 &= ~(SPI_CR1_DFF);
+
+    /* CRC calculation disabled */
+    obj->instance->CR1 &= ~(SPI_CR1_CRCEN);
+
+    /* Data phase (no CRC phase) */
+    obj->instance->CR1 &= ~(SPI_CR1_CRCNEXT);
+
+    /* 2-line unidirectional data mode selected */
+    obj->instance->CR1 &= ~(SPI_CR1_BIDIMODE);
 }
 
-void SpiSpeed(Spi_t* const obj, uint32_t frequencyHz)
+static uint32_t SpiSpeed(const Spi_t* const obj, uint32_t deriredFrequencyHz)
+{
+    uint32_t pclk = 0;
+    uint32_t prescaler = 0;
+    uint32_t brBits = 0;
+
+    if (obj->name == SPI_1 || obj->name == SPI_4 || obj->name == SPI_5)
+    {
+        /* APB2 */
+        uint32_t hclk = SystemCoreClock;
+        uint32_t apb2Prescaler = ((RCC->CFGR >> RCC_CFGR_PPRE2_Pos) & 0x7);
+        if (apb2Prescaler < 4)
+        {
+            apb2Prescaler = 1;
+        }
+        else
+        {
+            apb2Prescaler = 1 << (apb2Prescaler - 3);
+        }
+        pclk = hclk / apb2Prescaler;
+    }
+    else
+    {
+        /* APB1 */
+        uint32_t hclk = SystemCoreClock;
+        uint32_t apb1Prescaler = ((RCC->CFGR >> RCC_CFGR_PPRE1_Pos) & 0x7);
+        if (apb1Prescaler < 4)
+        {
+            apb1Prescaler = 1;
+        }
+        else
+        {
+            apb1Prescaler = 1 << (apb1Prescaler - 3);
+        }
+        pclk = hclk / apb1Prescaler;
+    }
+
+    prescaler = pclk / deriredFrequencyHz;
+
+    while ((prescaler > 2) && (brBits < 7))
+    {
+        prescaler >>= 1;
+        brBits++;
+    }
+
+    obj->instance->CR1 &= ~SPI_CR1_BR_Msk;
+    obj->instance->CR1 |= (brBits << SPI_CR1_BR_Pos);
+
+    return pclk / (1U << (brBits + 1));
+}
+
+static void SpiClockEnable(const Spi_t* const obj)
 {
     assert(obj != NULL);
 
-    uint32_t divisor = 0;
-    uint32_t spiClkFreq = SystemCoreClock;
-    uint32_t baud = 0;
-
-    while (spiClkFreq > frequencyHz)
+    switch (obj->name)
     {
-        divisor++;
-        spiClkFreq = (spiClkFreq >> 1);
-        if (divisor >= 7)
-        {
+        case SPI_1:
+            SPI_1_CLOCK_ENABLE;
             break;
-        }
+        case SPI_2:
+            SPI_2_CLOCK_ENABLE;
+            break;
+        case SPI_3:
+            SPI_3_CLOCK_ENABLE;
+            break;
+        case SPI_4:
+            SPI_4_CLOCK_ENABLE;
+            break;
+        case SPI_5:
+            SPI_5_CLOCK_ENABLE;
+            break;
+        default:
+            assert(0);
+            break;
     }
-    baud = (((divisor & 0x04) == 0) ? 0x00 : SPI_CR1_BR_2) |
-           (((divisor & 0x02) == 0) ? 0x00 : SPI_CR1_BR_1) |
-           (((divisor & 0x01) == 0) ? 0x00 : SPI_CR1_BR_0);
-
-    obj->instance->CR1 |= baud << SPI_CR1_BR_Pos;
 }
 
-void SpiEnable(Spi_t* const obj)
+static void SpiEnable(const Spi_t* const obj)
 {
     assert(obj != NULL);
 
     obj->instance->CR1 |= SPI_CR1_SPE;
 }
 
-void SpiInit(Spi_t* const obj, SPI_NAMES name, SPI_POLARITY polarity, SPI_PHASE phase, uint32_t frequencyHz)
+static void SpiDisable(const Spi_t* const obj)
 {
     assert(obj != NULL);
-    assert(frequencyHz != 0);
 
-    obj->name = name;
-    obj->initialized = false;
-
-    switch (name)
-    {
-        case SPI_1:
-            GpioInit(&m_gpio.mosi, PA_7, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_5);
-            GpioInit(&m_gpio.miso, PA_6, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_5);
-            GpioInit(&m_gpio.sck, PA_5, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_5);
-
-            SPI_1_CLOCK_ENABLE;
-
-            obj->instance = SPI1;
-
-            break;
-
-        case SPI_2:
-            GpioInit(&m_gpio.mosi, PB_15, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_5);
-            GpioInit(&m_gpio.miso, PB_14, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_5);
-            GpioInit(&m_gpio.sck, PB_13, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_5);
-
-            SPI_2_CLOCK_ENABLE;
-
-            obj->instance = SPI2;
-
-            break;
-
-        case SPI_3:
-            GpioInit(&m_gpio.mosi, PB_5, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_6);
-            GpioInit(&m_gpio.miso, PB_4, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_6);
-            GpioInit(&m_gpio.sck, PB_3, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_5);
-
-            SPI_3_CLOCK_ENABLE;
-
-            obj->instance = SPI3;
-
-            break;
-
-        case SPI_4:
-            GpioInit(&m_gpio.mosi, PA_1, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_5);
-            GpioInit(&m_gpio.miso, PA_11, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_6);
-            GpioInit(&m_gpio.sck, PB_13, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_6);
-
-            SPI_4_CLOCK_ENABLE;
-
-            obj->instance = SPI4;
-
-            break;
-
-        case SPI_5:
-            GpioInit(&m_gpio.mosi, PB_8, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_6);
-            GpioInit(&m_gpio.miso, PA_12, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_5);
-            GpioInit(&m_gpio.sck, PB_0, PIN_MODE_ALTERNATE, PIN_TYPE_NO_PULL_UP_PULL_DOWN, PIN_SPEED_FAST, PIN_CONFIG_PUSH_PULL, PIN_AF_6);
-
-            SPI_5_CLOCK_ENABLE;
-
-            obj->instance = SPI5;
-
-            break;
-
-        default:
-            assert(false);
-            break;
-    }
-
-    SpiMode(obj, polarity, phase);
-    SpiFormat(obj);
-    SpiSpeed(obj, frequencyHz);
-    SpiEnable(obj);
-
-    obj->initialized = true;
+    obj->instance->CR1 &= ~SPI_CR1_SPE;
 }
 
-void SpiTransmit(Spi_t* const obj, const uint8_t* const txBuffer, uint32_t size)
+static bool WaitFlagTimeout(volatile uint32_t* reg, uint32_t flag, bool state, uint32_t timeoutUs)
+{
+    while (((*reg & flag) ? 1 : 0) != (state ? 1 : 0))
+    {
+        if (timeoutUs == 0)
+        {
+            return false;
+        }
+        DelayUs(1);
+        timeoutUs--;
+    }
+    return true;
+}
+
+static void SpiClearOverrun(const Spi_t* const obj)
 {
     assert(obj != NULL);
-    assert(txBuffer != NULL);
 
-    uint8_t dummy = 0;
+    volatile uint32_t dummy = 0;
 
-    if (!obj->initialized)
-    {
-        return;
-    }
-
-    for (uint8_t i = 0; i < size; i++)
-    {
-        while (!(obj->instance->SR & SPI_SR_TXE));
-
-        obj->instance->DR = txBuffer[i];
-
-        while (obj->instance->SR & SPI_SR_BSY);
-
-        dummy = obj->instance->DR;
-        dummy = obj->instance->SR;
-    }
+    dummy = obj->instance->DR;
+    IGNORE(dummy);
+    dummy = obj->instance->SR;
+    IGNORE(dummy);
 }
-
-void SpiReceive(Spi_t* const obj, uint8_t* const rxBuffer, uint32_t size)
-{
-    assert(obj != NULL);
-    assert(rxBuffer != NULL);
-
-    uint8_t dummy = 0xFF;
-
-    if (!obj->initialized)
-    {
-        return;
-    }
-
-    for (uint8_t i = 0; i < size; i++)
-    {
-        obj->instance->DR = dummy;
-
-        while (!(obj->instance->SR & SPI_SR_RXNE));
-
-        rxBuffer[i] = obj->instance->DR;
-    }
-}
-
-void SpiTransmitReceive(Spi_t* const obj, const uint8_t* const txBuffer, uint8_t* const rxBuffer, uint32_t size)
-{
-    assert(obj != NULL);
-    assert(txBuffer != NULL);
-    assert(rxBuffer != NULL);
-
-    uint8_t dummy = 0xFF;
-
-    if (!obj->initialized)
-    {
-        return;
-    }
-
-    for (uint8_t i = 0; i < size; i++)
-    {
-        while (!(obj->instance->SR & SPI_SR_TXE));
-
-        obj->instance->DR = txBuffer[i];
-
-        while (obj->instance->SR & SPI_SR_BSY);
-
-        while (!(obj->instance->SR & SPI_SR_RXNE));
-
-        rxBuffer[i] = obj->instance->DR;
-
-        dummy = obj->instance->SR;
-    }
-}
-
--- spi/spi.h
+++ spi/spi.h
@@ -6,6 +6,8 @@
 
 #include "stm32f411xe.h"
 
+#include "gpio.h"
+
 typedef enum
 {
     SPI_1 = 0,
@@ -30,8 +32,17 @@ typedef enum
 
 typedef struct
 {
+    Gpio_t miso;
+    Gpio_t mosi;
+    Gpio_t sck;
+    Gpio_t nss;
+} SpiGpio_t;
+
+typedef struct
+{
     SPI_TypeDef* instance;
     SPI_NAMES name;
+    SpiGpio_t gpio;
     bool initialized;
 } Spi_t;
 
@@ -40,34 +51,25 @@ typedef struct
  * [in] - name - SPI name
  * [in] - polarity - SPI clock polarity
  * [in] - phase - SPI clock phase
- * [in] - frequencyHz - SPI frequency in Hz
+ * [in] - deriredFrequencyHz - SPI frequency in Hz
+ * NOTE: actual SPI frequency might be differ to desired
  * [out] - none
  * */
-void SpiInit(Spi_t* const obj, SPI_NAMES name, SPI_POLARITY polarity, SPI_PHASE phase, uint32_t frequencyHz);
+void SpiInit(Spi_t* const obj, SPI_NAMES name, SPI_POLARITY polarity, SPI_PHASE phase, uint32_t deriredFrequencyHz);
 
-/*Brief: SPI transmit
+/*Brief: SPI de-initialization
  * [in] - obj - pointer to SPI object
- * [in] - txBuffer - pointer to tx buffer
- * [in] - size - tx buffer size
  * [out] - none
  * */
-void SpiTransmit(Spi_t* const obj, const uint8_t* const txBuffer, uint32_t size);
-
-/*Brief: SPI receive
- * [in] - obj - pointer to SPI object
- * [in] - rxBuffer - pointer to rx buffer
- * [in] - size - rx buffer size
- * [out] - none
- * */
-void SpiReceive(Spi_t* const obj, uint8_t* const rxBuffer, uint32_t size);
+void SpiDeinit(Spi_t* const obj);
 
 /*Brief: SPI transmit/receive
  * [in] - obj - pointer to SPI object
- * [in] - txBuffer - pointer to tx buffer
- * [in] - rxBuffer - pointer to rx buffer
- * [in] - size - rx buffer size
- * [out] - none
+ * [in] - txBuffer - buffer to transmit
+ * [in] - rxBuffer - buffer to receive
+ * [in] - size - buffer size
+ * [out] - true - transfer successful; false - otherwise
  * */
-void SpiTransmitReceive(Spi_t* const obj, const uint8_t* const txBuffer, uint8_t* const rxBuffer, uint32_t size);
+bool SpiTransfer(Spi_t* const obj, const uint8_t* const txBuffer, uint8_t* const rxBuffer, uint8_t size);
 
 #endif /* SPI_H */
