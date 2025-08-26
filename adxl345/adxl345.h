58498a18f6914f239fa2c5c1c80498fee1803b65
jura <jglotovyn@gmail.com>
2025-08-26 20:23:05 +0300

    ADD: adxl345 driver

--- adxl345/adxl345.h
+++ adxl345/adxl345.h
@@ -3,9 +3,27 @@
 
 #include <stdint.h>
 
+typedef void (*ADXL_RequestHandler_t)(uint8_t value, void* context);
+
+/*Brief: ADXL345 initialization
+ * [in] - none
+ * [out] - none
+ * */
 void ADXL_Init(void);
-void ADXL_GetId(uint8_t* const id);
-void ADXL_GetVector(void);
-void ADXL_DumpRegisters(void);
+
+/*Brief: ADXL345 read single register async
+ * [in] - address - register address
+ * [in] - callback - callback function on receive done
+ * [in] - context - response storage address
+ * [out] - none
+ * */
+void ADXL_ReadRegisterAsync(uint8_t address, ADXL_RequestHandler_t callback, void* context);
+
+/*Brief: ADXL345 write single register async
+ * [in] - address - register address
+ * [in] - value - new value
+ * [out] - none
+ * */
+void ADXL_WriteRegisterAsync(uint8_t address, void* value);
 
 #endif /* ADXL345_H */
