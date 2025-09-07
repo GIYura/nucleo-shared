54e7a111fb5cc8bc0dbe69291f561c70335f6b96
jura <jglotovyn@gmail.com>
2025-09-07 15:04:15 +0300

    ADD: event queue

---
+++ event/event.c
@@ -0,0 +1,27 @@
+#include <stddef.h>
+
+#include "assert.h"
+#include "event.h"
+#include "buffer.h"
+
+static Buffer_t m_eventQueue;
+static Event_t m_events[EVENT_QUEUE_SIZE +1];
+
+void EventQueueInit(void)
+{
+    BufferCreate(&m_eventQueue, m_events, sizeof(m_events), sizeof(Event_t), true);
+}
+
+bool EventQueue_Enqueue(const Event_t* const event)
+{
+    ASSERT(event != NULL);
+
+    return BufferPut(&m_eventQueue, event, sizeof(Event_t));
+}
+
+bool EventQueue_Dequeue(Event_t* const event)
+{
+    ASSERT(event != NULL);
+
+    return BufferGet(&m_eventQueue, event, sizeof(Event_t));
+}
---
+++ event/event.h
@@ -0,0 +1,41 @@
+#ifndef EVENT_H
+#define EVENT_H
+
+#include <stdbool.h>
+
+#define EVENT_QUEUE_SIZE    15
+
+typedef enum
+{
+    EVENT_NONE = 0,
+    EVENT_ACCEL_VECTOR_READY,
+    EVENT_ACCEL_ID_READY,
+    EVENT_BUTTON_PRESSED,
+    EVENT_TIMER_TICK,
+} EVENT_TYPE;
+
+typedef struct
+{
+    EVENT_TYPE type;
+    void* context;
+} Event_t;
+
+/*Brief: Event queue initialization
+ * [in] - none
+ * [out] - none
+ * */
+void EventQueueInit(void);
+
+/*Brief: Put event into the queue
+ * [in] - pointer to event
+ * [out] - true - event successfully sent to queue; false - otherwise (queue is full)
+ * */
+bool EventQueue_Enqueue(const Event_t* const event);
+
+/*Brief: Retrieve event from the queue
+ * [in] - pointer to event
+ * [out] - true - event successfully retrieved from queue; false - otherwise (queue is empty)
+ * */
+bool EventQueue_Dequeue(Event_t* const event);
+
+#endif /* EVENT_H */
