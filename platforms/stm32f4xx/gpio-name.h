a74bd701b17103c76c7917c369482471666a571e
jura <jglotovyn@gmail.com>
2025-11-26 20:30:35 +0200

    ADD: alternate function names

--- platforms/stm32f4xx/gpio-name.h
+++ platforms/stm32f4xx/gpio-name.h
@@ -13,6 +13,27 @@ typedef enum
     PD_0, PD_1, PD_2, PD_3, PD_4, PD_5, PD_6, PD_7, PD_8, PD_9, PD_10, PD_11, PD_12, PD_13, PD_14, PD_15,
     PE_0, PE_1, PE_2, PE_3, PE_4, PE_5, PE_6, PE_7, PE_8, PE_9, PE_10, PE_11, PE_12, PE_13, PE_14, PE_15,
     PH_0, PH_1, PH_2, PH_3, PH_4, PH_5, PH_6, PH_7, PH_8, PH_9, PH_10, PH_11, PH_12, PH_13, PH_14, PH_15,
-} PIN_NAMES;
+} STM32_PIN_NAMES;
+
+/* Alternate gpio */
+typedef enum
+{
+    PIN_AF_0 = 0,
+    PIN_AF_1,
+    PIN_AF_2,
+    PIN_AF_3,
+    PIN_AF_4,
+    PIN_AF_5,
+    PIN_AF_6,
+    PIN_AF_7,
+    PIN_AF_8,
+    PIN_AF_9,
+    PIN_AF_10,
+    PIN_AF_11,
+    PIN_AF_12,
+    PIN_AF_13,
+    PIN_AF_14,
+    PIN_AF_15,
+} STM32_PIN_AF;
 
 #endif /* GPIO_NAME_H */
