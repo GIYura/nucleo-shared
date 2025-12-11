d60a63a7ee54fa928d92e75f486e1a954ad0ef8e
jura <jglotovyn@gmail.com>
2025-12-11 21:29:37 +0200

    ADD: mcu-specific gpio implementation

--- platforms/cc32xx/gpio.c
+++ platforms/cc32xx/gpio.c
@@ -7,13 +7,14 @@
 
 #include <ti/devices/cc32xx/inc/hw_types.h>
 #include <ti/devices/cc32xx/inc/hw_memmap.h>
+#include <ti/devices/cc32xx/inc/hw_ints.h>
 
 #include <ti/devices/cc32xx/driverlib/gpio.h>
 #include <ti/devices/cc32xx/driverlib/pin.h>
 #include <ti/devices/cc32xx/driverlib/prcm.h>
 #include <ti/devices/cc32xx/driverlib/interrupt.h>
 
-static GpioHandle_t* m_GpioIrq[PIN_GPIO_MAX] = {0};
+static GpioHandle_t* m_GpioIrq[PIN_GPIO_MAX] = { NULL };
 
 typedef struct
 {
@@ -23,42 +24,65 @@ typedef struct
 } GpioMap_t;
 
 static const GpioMap_t m_GPIO_MAP[PIN_GPIO_MAX] = {
-        [PIN_GPIOA0_0] = { GPIOA0_BASE, GPIO_PIN_0, PIN_50 },
-        [PIN_GPIOA0_1] = { GPIOA0_BASE, GPIO_PIN_1, PIN_55 },
-        [PIN_GPIOA0_2] = { GPIOA0_BASE, GPIO_PIN_2, PIN_57 },
-        [PIN_GPIOA0_3] = { GPIOA0_BASE, GPIO_PIN_3, PIN_58 },
-        [PIN_GPIOA0_4] = { GPIOA0_BASE, GPIO_PIN_4, PIN_59 },
-        [PIN_GPIOA0_5] = { GPIOA0_BASE, GPIO_PIN_5, PIN_60 },
-        [PIN_GPIOA0_6] = { GPIOA0_BASE, GPIO_PIN_6, PIN_61 },
-        [PIN_GPIOA0_7] = { GPIOA0_BASE, GPIO_PIN_7, PIN_62 },
-
-        [PIN_GPIOA1_0] = { GPIOA1_BASE, GPIO_PIN_0, PIN_63 },
-        [PIN_GPIOA1_1] = { GPIOA1_BASE, GPIO_PIN_1, PIN_64 },
-        [PIN_GPIOA1_2] = { GPIOA1_BASE, GPIO_PIN_2, PIN_01 },
-        [PIN_GPIOA1_3] = { GPIOA1_BASE, GPIO_PIN_3, PIN_02 },
-        [PIN_GPIOA1_4] = { GPIOA1_BASE, GPIO_PIN_4, PIN_03 },
-        [PIN_GPIOA1_5] = { GPIOA1_BASE, GPIO_PIN_5, PIN_04 },
-        [PIN_GPIOA1_6] = { GPIOA1_BASE, GPIO_PIN_6, PIN_05 },
-        [PIN_GPIOA1_7] = { GPIOA1_BASE, GPIO_PIN_7, PIN_06 },
-
-        [PIN_GPIOA2_0] = { GPIOA2_BASE, GPIO_PIN_0, PIN_07 },
-        [PIN_GPIOA2_1] = { GPIOA2_BASE, GPIO_PIN_1, PIN_08 },
-
-       [PIN_GPIOA2_6] = { GPIOA2_BASE, GPIO_PIN_6, PIN_15 },
-       [PIN_GPIOA2_7] = { GPIOA2_BASE, GPIO_PIN_7, PIN_16 },
-
-       [PIN_GPIOA3_0] = { GPIOA3_BASE, GPIO_PIN_0, PIN_17 },
-       [PIN_GPIOA3_1] = { GPIOA3_BASE, GPIO_PIN_1, PIN_21 },
-
-       [PIN_GPIOA3_4] = { GPIOA3_BASE, GPIO_PIN_4, PIN_18 },
-       [PIN_GPIOA3_5] = { GPIOA3_BASE, GPIO_PIN_5, PIN_20 },
-       [PIN_GPIOA3_6] = { GPIOA3_BASE, GPIO_PIN_6, PIN_53 },
-       [PIN_GPIOA3_7] = { GPIOA3_BASE, GPIO_PIN_7, PIN_45 },
+    [PIN_GPIOA0_0] = { GPIOA0_BASE, GPIO_PIN_0, PIN_50 },
+    [PIN_GPIOA0_1] = { GPIOA0_BASE, GPIO_PIN_1, PIN_55 },
+    [PIN_GPIOA0_2] = { GPIOA0_BASE, GPIO_PIN_2, PIN_57 },
+    [PIN_GPIOA0_3] = { GPIOA0_BASE, GPIO_PIN_3, PIN_58 },
+    [PIN_GPIOA0_4] = { GPIOA0_BASE, GPIO_PIN_4, PIN_59 },
+    [PIN_GPIOA0_5] = { GPIOA0_BASE, GPIO_PIN_5, PIN_60 },
+    [PIN_GPIOA0_6] = { GPIOA0_BASE, GPIO_PIN_6, PIN_61 },
+    [PIN_GPIOA0_7] = { GPIOA0_BASE, GPIO_PIN_7, PIN_62 },
+    [PIN_GPIOA1_0] = { GPIOA1_BASE, GPIO_PIN_0, PIN_63 },
+    [PIN_GPIOA1_1] = { GPIOA1_BASE, GPIO_PIN_1, PIN_64 },
+    [PIN_GPIOA1_2] = { GPIOA1_BASE, GPIO_PIN_2, PIN_01 },
+    [PIN_GPIOA1_3] = { GPIOA1_BASE, GPIO_PIN_3, PIN_02 },
+    [PIN_GPIOA1_4] = { GPIOA1_BASE, GPIO_PIN_4, PIN_03 },
+    [PIN_GPIOA1_5] = { GPIOA1_BASE, GPIO_PIN_5, PIN_04 },
+    [PIN_GPIOA1_6] = { GPIOA1_BASE, GPIO_PIN_6, PIN_05 },
+    [PIN_GPIOA1_7] = { GPIOA1_BASE, GPIO_PIN_7, PIN_06 },
+    [PIN_GPIOA2_0] = { GPIOA2_BASE, GPIO_PIN_0, PIN_07 },
+    [PIN_GPIOA2_1] = { GPIOA2_BASE, GPIO_PIN_1, PIN_08 },
+    [PIN_GPIOA2_6] = { GPIOA2_BASE, GPIO_PIN_6, PIN_15 },
+    [PIN_GPIOA2_7] = { GPIOA2_BASE, GPIO_PIN_7, PIN_16 },
+    [PIN_GPIOA3_0] = { GPIOA3_BASE, GPIO_PIN_0, PIN_17 },
+    [PIN_GPIOA3_1] = { GPIOA3_BASE, GPIO_PIN_1, PIN_21 },
+    [PIN_GPIOA3_4] = { GPIOA3_BASE, GPIO_PIN_4, PIN_18 },
+    [PIN_GPIOA3_5] = { GPIOA3_BASE, GPIO_PIN_5, PIN_20 },
+    [PIN_GPIOA3_6] = { GPIOA3_BASE, GPIO_PIN_6, PIN_53 },
+    [PIN_GPIOA3_7] = { GPIOA3_BASE, GPIO_PIN_7, PIN_45 },
 };
 
 static uint8_t GpioGetPinIndex(uint32_t base, uint8_t pinMask)
 {
     /*TODO:*/
+    return 0;
+}
+
+static void GpioDisableClocks(uint32_t base)
+{
+    switch (base)
+    {
+        case GPIOA0_BASE:
+            PRCMPeripheralClkDisable(PRCM_GPIOA0, PRCM_RUN_MODE_CLK);
+            break;
+
+        case GPIOA1_BASE:
+            PRCMPeripheralClkDisable(PRCM_GPIOA1, PRCM_RUN_MODE_CLK);
+            break;
+
+        case GPIOA2_BASE:
+            PRCMPeripheralClkDisable(PRCM_GPIOA2, PRCM_RUN_MODE_CLK);
+            break;
+
+        case GPIOA3_BASE:
+            PRCMPeripheralClkDisable(PRCM_GPIOA3, PRCM_RUN_MODE_CLK);
+            break;
+
+        default:
+            /* should never reach here */
+            ASSERT(false);
+            break;
+    }
 }
 
 static void GpioEnableClocks(uint32_t base)
@@ -158,7 +182,78 @@ static uint32_t GpioMapEdge(PIN_IRQ_MODES mode)
     }
 }
 
-void GpioInit(GpioHandle_t* const handle,
+static uint8_t GpioMapInterruptPriority(uint8_t priority)
+{
+    switch (priority)
+    {
+        case 0:
+            return INT_PRIORITY_LVL_0;
+            break;
+
+        case 1:
+            return INT_PRIORITY_LVL_1;
+            break;
+
+        case 2:
+            return INT_PRIORITY_LVL_2;
+            break;
+
+        case 3:
+            return INT_PRIORITY_LVL_3;
+            break;
+
+        case 4:
+            return INT_PRIORITY_LVL_4;
+            break;
+
+        case 5:
+            return INT_PRIORITY_LVL_5;
+            break;
+
+        case 6:
+            return INT_PRIORITY_LVL_6;
+            break;
+
+        case 7:
+            return INT_PRIORITY_LVL_7;
+            break;
+
+        default:
+            /* never reach here */
+            ASSERT(false);
+            return 0xFF;
+            break;
+    }
+}
+
+static uint8_t GpioMapInterrupt(uint32_t base)
+{
+    switch (base)
+    {
+        case GPIOA0_BASE:
+            return INT_GPIOA0;
+            break;
+
+        case GPIOA1_BASE:
+            return INT_GPIOA1;
+            break;
+
+        case GPIOA2_BASE:
+            return INT_GPIOA2;
+            break;
+
+        case GPIOA3_BASE:
+            return INT_GPIOA3;
+            break;
+
+        default:
+            ASSERT(false);
+            return 0xFF;
+            break;
+    }
+}
+
+static void GpioOpen(GpioHandle_t* const handle,
               uint8_t pin,
               PIN_MODES mode,
               PIN_TYPES pull,
@@ -167,6 +262,7 @@ void GpioInit(GpioHandle_t* const handle,
               uint32_t value)
 {
     ASSERT(handle != NULL);
+    ASSERT(handle->ops != NULL);
     ASSERT(pin < PIN_GPIO_MAX);
 
     if (pin == PIN_NC)
@@ -176,8 +272,8 @@ void GpioInit(GpioHandle_t* const handle,
 
     const GpioMap_t* map = &m_GPIO_MAP[pin];
 
-    handle->hw.cc3220.base = map->base;
-    handle->hw.cc3220.pinMask = map->mask;
+    handle->gpio.cc3220.base = map->base;
+    handle->gpio.cc3220.pinMask = map->mask;
     handle->irqHandler = NULL;
 
     GpioEnableClocks(map->base);
@@ -219,60 +315,102 @@ void GpioInit(GpioHandle_t* const handle,
             ASSERT(false);
             break;
     }
+
+    handle->initialized = true;
 }
 
-void GpioWrite(const GpioHandle_t* const handle, PIN_STATES state)
+static void GpioClose(GpioHandle_t* const handle)
+{
+    if ((handle == NULL) || (handle->ops != NULL))
+    {
+        return;
+    }
+
+    handle->initialized = false;
+
+    GpioDisableClocks(handle->gpio.cc3220.base);
+
+    /*TODO: disable interrupts */
+}
+
+static void GpioWrite(const GpioHandle_t* const handle, PIN_STATES state)
 {
     ASSERT(handle != NULL);
+    ASSERT(handle->ops != NULL);
 
-    uint32_t base  = handle->hw.cc3220.base;
-    uint8_t pinMask = handle->hw.cc3220.pinMask;
+    if (!handle->initialized)
+    {
+        return;
+    }
+
+    uint32_t base = handle->gpio.cc3220.base;
+    uint8_t pinMask = handle->gpio.cc3220.pinMask;
 
     uint8_t value = (state == PIN_STATE_HIGH) ? pinMask : 0U;
 
     GPIOPinWrite(base, pinMask, value);
 }
 
-uint16_t GpioRead(const GpioHandle_t* const handle)
+static uint16_t GpioRead(const GpioHandle_t* const handle)
 {
     ASSERT(handle != NULL);
+    ASSERT(handle->ops != NULL);
 
-    uint32_t base = handle->hw.cc3220.base;
-    uint8_t pinMask = handle->hw.cc3220.pinMask;
+    if (!handle->initialized)
+    {
+        return 0xFF;
+    }
+
+    uint32_t base = handle->gpio.cc3220.base;
+    uint8_t pinMask = handle->gpio.cc3220.pinMask;
 
     return (uint16_t)(GPIOPinRead(base, pinMask) ? 1U : 0U);
 }
 
-void GpioToggle(const GpioHandle_t* const handle)
+static void GpioToggle(const GpioHandle_t* const handle)
 {
     ASSERT(handle != NULL);
+    ASSERT(handle->ops != NULL);
 
-    uint32_t base = handle->hw.cc3220.base;
-    uint8_t pinMask = handle->hw.cc3220.pinMask;
+    if (!handle->initialized)
+    {
+        return;
+    }
 
+    uint32_t base = handle->gpio.cc3220.base;
+    uint8_t pinMask = handle->gpio.cc3220.pinMask;
+#if 0
     uint32_t primask = __get_PRIMASK();
     __disable_irq();
+#endif
 
     uint32_t value = GPIOPinRead(base, pinMask);
     uint8_t newValue = (value ? 0U : pinMask);
 
     GPIOPinWrite(base, pinMask, newValue);
-
+#if 0
     __set_PRIMASK(primask);
+#endif
 }
 
-void GpioSetInterrupt(GpioHandle_t* const handle, PIN_IRQ_MODES mode, uint8_t priority, GpioIrqHandler handler)
+static void GpioSetInterrupt(GpioHandle_t* const handle, PIN_IRQ_MODES mode, uint8_t priority, GpioIrqHandler handler)
 {
+    /* TODO: */
     ASSERT(handle != NULL);
+    ASSERT(handle->ops != NULL);
 
     if (mode == PIN_IRQ_NONE)
     {
         return;
     }
 
-    uint32_t base = handle->hw.cc3220.base;
-    uint8_t pinMask = handle->hw.cc3220.pinMask;
+    uint32_t base = handle->gpio.cc3220.base;
+    uint8_t pinMask = handle->gpio.cc3220.pinMask;
     uint8_t pinIndex = GpioGetPinIndex(base, pinMask);
+    uint8_t intPriority = GpioMapInterruptPriority(priority);
+    uint8_t intNum = GpioMapInterrupt(base);
+
+    handle->irqHandler = handler;
 
     GPIOIntDisable(base, pinMask);
     GPIOIntClear(base, pinMask);
@@ -281,18 +419,34 @@ void GpioSetInterrupt(GpioHandle_t* const handle, PIN_IRQ_MODES mode, uint8_t pr
 
     GPIOIntTypeSet(base, pinMask, edge);
 
-    handle->irqHandler = handler;
+    GPIOIntClear(base, pinMask);
 
+    GPIOIntEnable(base, pinMask);
+
+    IntPrioritySet(intNum, intPriority);
+
+    IntRegister(intNum, handler);
+
+    IntEnable(intNum);
+
+#if 0
     for (uint8_t i = 0; i < PIN_GPIO_MAX; i++)
     {
-        if (m_GpioIrq[i] != NULL && m_GpioIrq[i]->hw.cc3220.pinIndex == pinIndex)
+        if (m_GpioIrq[i] != NULL && m_GpioIrq[i]->gpio.cc3220.pinIndex == pinIndex)
         {
             return;
         }
     }
+#endif
 }
 
-
-
-
+/* Gpio operations */
+const GpioOps_t g_GpioOps = {
+    .open = &GpioOpen,
+    .close = &GpioClose,
+    .read = &GpioRead,
+    .write = &GpioWrite,
+    .toggle = &GpioToggle,
+    .interrupt = &GpioSetInterrupt
+};
 
